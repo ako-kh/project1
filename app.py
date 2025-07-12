764e321bba22f589134169926c121b665c21d80a
MAMAMI333 <xoravaakaki989@gmail.com>
2025-07-12 17:39:59 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/about_us")
+def about_us():
+    return render_template("about_us.html")
